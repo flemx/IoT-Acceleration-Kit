9f9354213a066429151a7872b6271be99ba3ff4c
Damien Fleminks <damien@fleminks.com>
2019-09-09 21:36:51 +0100

    Cleaning up code

--- force-app/main/default/lwc/vehicleMeter/vehicleMeter.js
+++ force-app/main/default/lwc/vehicleMeter/vehicleMeter.js
@@ -1,5 +1,5 @@
 import { LightningElement, track, api } from 'lwc';
-import { subscribe, isEmpEnabled } from 'lightning/empApi';
+import { subscribe } from 'lightning/empApi';
 import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
 import CHARTJS from '@salesforce/resourceUrl/chartjsStream';
 
@@ -8,29 +8,27 @@ import CHARTJS from '@salesforce/resourceUrl/chartjsStream';
 
 export default class VehicleMeter extends LightningElement {
 
-    //Set platform event
-    @api channelName;
+    // Set color of temperature text
     @track classNameTemp = 'temp-reading-green';
 
-    //Track last platform event send
-    isLastSend = false;
-
-    //Chart parameters
+    //Chart public properties
     @api yLength;
-    @api yStep;
     @api fakeMin;
     @api fakeMax;
     @api isFake;
     @api yellowStatus;
     @api redStatus;
     @api eventParameter;
+    @api channelName;
     
-    //The platform event value
-    @track eventValue;
-
-    @track isSubscribeDisabled = false;
-    @track isUnsubscribeDisabled = !this.isSubscribeDisabled;
+    //The platform event value & empApi subscription object
+    @track eventValue
     subscription = {};
+
+    // Object for chart parameters
+    chartConfig = {};
+
+    // Boolean to track if renderedCallback() was executed
     chartInitialized = false;
 
     // Tracks changes to channelName text field
@@ -44,14 +42,12 @@ export default class VehicleMeter extends LightningElement {
         if (this.chartInitialized) {
             return;
         }
-        console.log('Can emppApi be used?');
-        console.log(isEmpEnabled);
-        window.yLength = parseFloat(this.yLength);
-        window.yStep = parseFloat(this.yStep);
-        window.fakeMin = parseFloat(this.fakeMin);
-        window.fakeMax = parseFloat(this.fakeMax);
-        window.yellowStatus = parseFloat(this.yellowStatus);
-        window.redStatus = parseFloat(this.redStatus);
+
+        this.chartConfig.yLength = parseFloat(this.yLength);
+        this.chartConfig.fakeMin = parseFloat(this.fakeMin);
+        this.chartConfig.fakeMax = parseFloat(this.fakeMax);
+        this.chartConfig.yellowStatus = parseFloat(this.yellowStatus);
+        this.chartConfig.redStatus = parseFloat(this.redStatus);
         
         //this.isFake = this.isFake;
         this.chartInitialized = true;
@@ -74,10 +70,8 @@ export default class VehicleMeter extends LightningElement {
         //Handle platform events 
         console.log('renderedCallback executed');
         const messageCallback = (response) => {
-            console.log(this.eventParameter + ' is : ', response.data.payload[this.eventParameter]);
+            console.log('Event received - ' + this.eventParameter + ' is : ', response.data.payload[this.eventParameter]);
             this.eventValue = parseFloat(response.data.payload[this.eventParameter]);
-            //this.isLastSend = true;
-            //window.myVar = this.eventValue;
             // Response contains the payload of the new message received
         };
 
@@ -113,24 +107,14 @@ export default class VehicleMeter extends LightningElement {
         //Variable to keep track when the first bar is loaded
         let firstNumber = true;
 
+        // Color codes to use for chart bars
         let chartColors = {
-            red: 'rgb(149, 235, 91)',
-            yellow: 'rgb(255, 242, 102)',
-            green: 'rgb(255, 84, 54)',
-            darkRed: 'rgb(69, 110, 42)',
-            darkYellow: 'rgb(150, 143, 59)',
-            darkGreen: 'rgb(133, 43, 28)'
+            green: 'rgb(149, 235, 91)',
+            red: 'rgb(255, 84, 54)',
+            darkGreen: 'rgb(69, 110, 42)',
+            darkRed: 'rgb(133, 43, 28)'
         };
 
-        // let chartColors = {
-        //     red: 'rgb(255, 84, 54)',
-        //     yellow: 'rgb(255, 242, 102',
-        //     green: 'rgb(149, 235, 91',
-        //     darkRed: 'rgb(133, 43, 28)',
-        //     darkYellow: 'rgb(150, 143, 59',
-        //     darkGreen: 'rgb(69, 110, 42)'
-        // };
-   
         
         //Below function will be called to set the data for the y ass and dynamically set the barchart colors 
         let randomScalingFactor = () => {
@@ -144,21 +128,16 @@ export default class VehicleMeter extends LightningElement {
 
 
             let setBarcolor = (length) =>{
-                let lowRed = Math.round( (window.redStatus - 0.1) * 10) / 10;
                 window.myChart.config.data.datasets[0].backgroundColor = [];
                 window.myChart.config.data.datasets[0].borderColor = [];
                 for(let i = 0; i < length; i++){
                     let valueArr = window.myChart.config.data.datasets[0].data;
                     for(let j = 0; j < length; j++){
-                        if(valueArr[j].y < window.redStatus){
+                        if(valueArr[j].y > this.chartConfig.redStatus){
                             window.myChart.config.data.datasets[0].backgroundColor.push(chartColors.red);
                             window.myChart.config.data.datasets[0].borderColor.push(chartColors.darkRed);
                         }
-                        if(valueArr[j].y > lowRed && valueArr[j].y < (window.yellowStatus + 0.1)){
-                            window.myChart.config.data.datasets[0].backgroundColor.push(chartColors.yellow);
-                            window.myChart.config.data.datasets[0].borderColor.push(chartColors.darkYellow);
-                        }
-                        if(valueArr[j].y > window.yellowStatus){
+                        if(valueArr[j].y < this.chartConfig.redStatus + 0.1){
                             window.myChart.config.data.datasets[0].backgroundColor.push(chartColors.green);
                             window.myChart.config.data.datasets[0].borderColor.push(chartColors.darkGreen);
                         }
@@ -169,51 +148,37 @@ export default class VehicleMeter extends LightningElement {
             if(firstNumber){
                 firstNumber = false;
                 if(this.isFake){
-                    //window.myVar = 2.1;
-                    this.eventValue = 2.1;
+                    this.eventValue = this.chartConfig.redStatus;
                 }else{
-                    //window.myVar = 0.0;
                     this.eventValue = 0.0;
                 }
-                //setBarcolor(window.myVar,1);
             }
             if(!firstNumber){
                 
                 if(this.isFake){
-                   // window.myVar = Math.round( (Math.random() * (window.fakeMax - window.fakeMin) + window.fakeMin) * 10) / 10;
-                    this.eventValue  = Math.round( (Math.random() * (window.fakeMax - window.fakeMin) + window.fakeMin) * 10) / 10;
+                    this.eventValue  = Math.round( (Math.random() * (this.chartConfig.fakeMax - this.chartConfig.fakeMin) + this.chartConfig.fakeMin) * 10) / 10;
                 }
                 
                 let length = window.myChart.config.data.datasets[0].data.length - 1;
-                //console.log('length is: ', length);
                 setBarcolor(length);
             }
-    
-            
-                if(this.eventValue > 7){
+                if(this.eventValue > this.chartConfig.redStatus){
                     this.classNameTemp = 'temp-reading-red';
+                    console.log(" this.classNameTemp = 'temp-reading-red';")
+                    console.log('this.classNameTemp: ' + this.eventValue);
+                    console.log('this.chartConfig.redStatus: ' + this.chartConfig.redStatus);
                 }
-                if(this.eventValue < 8){
+                if(this.eventValue < this.chartConfig.redStatus + 1){
                     this.classNameTemp = 'temp-reading-green';
+                    console.log(" this.classNameTemp = 'temp-reading-green';")
+                    console.log('this.classNameTemp: ' + this.eventValue);
+                    console.log('this.chartConfig.redStatus: ' + this.chartConfig.redStatus);
                 }
                 return this.eventValue;
             
         }
 
-        // Workaround to keep bars allignes as timedelay is inconsistent over time 
-        /*
-        let firsDate = false;
-        let DateBefore;
-        function handleDate(){
-            if(!firsDate){
-                firsDate = true;
-                DateBefore = Date.now();
-                return DateBefore;
-            }
-            DateBefore = DateBefore +1300;
-            return DateBefore;
-        }
-        */
+
 
         let onRefresh = (chart) => {
             chart.config.data.datasets.forEach(function(dataset) {
@@ -263,9 +228,9 @@ export default class VehicleMeter extends LightningElement {
                         display: true,
                         ticks: {
                             beginAtZero: true,
-                            steps: window.yStep,
-                            stepValue: window.yStep,
-                            max: window.yLength
+                            steps: 0.5,
+                            stepValue: 0.5,
+                            max: this.chartConfig.yLength
                         },
                         scaleLabel: {
                             display: true,
