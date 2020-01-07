1f5172b889f1c7fe803e98b652668170bd5a9a5c
Damien <damien@fleminks.com>
2020-01-07 11:45:18 +0000

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sfdx force:org:create -f config/project-scratch-def.json -s -d 1
-sfdx force:source:push
+sfdx sfdx force:auth:web:login -s 
+sfdx force:source:deploy   -p force-app/main/default/ 
